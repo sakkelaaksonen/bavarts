f4ad43cb4a94050993cfc49b17e4793910866758
Sakari Laaksonen <sakke.laaksonen@gmail.com>
2025-08-25 23:19:52 +0300

    update descriptions

--- src/_data/products.js
+++ src/_data/products.js
@@ -9,15 +9,15 @@ export default {
         name: "Calabash Lamp 1",
         price: 400,
         id: "1",
-        description: "This is the first product",
-        image: "assets/lamp1-3.jpg",
+        description: "Medium sized calabash lamp with star shaped cutouts.",
+        image: "assets/lamp1-3.webp",
         images: ["assets/lamp1-night.webp", "assets/lamp1-day.webp"]
     },
     {
         name: "Calabash Lamp 2",
         price: 200,
         id: "2",
-        description: "This is the second product",
+        description: "Medium sized calabash lamp with a natural finish and scale shape cutouts.",
         image: "assets/lamp2-day.webp",
         images: ["assets/lamp2-day.webp", "assets/lamp2-day-2.webp"]
     },
@@ -25,7 +25,7 @@ export default {
         name: "Calabash Lamp 3",
         price: 200,
         id: "3",
-        description: "This is the third product",
+        description: "Medium sized calabash lamp with elephant motifs.",
         image: "assets/lamp3-day.webp",
         images: ["assets/lamp3-day.webp", "assets/lamp3-day-2.webp"]
     }
